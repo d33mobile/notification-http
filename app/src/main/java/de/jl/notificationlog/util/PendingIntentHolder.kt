19ab25b2ead70d9581cf4a61d35101fc7f6ba744
Jonas L <jonas@determapp.de>
2019-01-20 08:48:47 +0100

    Add click action deduplication

--- app/src/main/java/de/jl/notificationlog/util/PendingIntentHolder.kt
+++ app/src/main/java/de/jl/notificationlog/util/PendingIntentHolder.kt
@@ -1,17 +1,65 @@
 package de.jl.notificationlog.util
 
 import android.app.PendingIntent
-import androidx.collection.LruCache
+import android.util.Log
+import de.jl.notificationlog.BuildConfig
 
 object PendingIntentHolder {
-    private val data = LruCache<Long, PendingIntent>(64)
+    private const val LOG_TAG = "PendingIntentHolder"
+    private const val CACHE_SIZE = 64
+
+    private var nextCachedIntentId = 0
+    private val cachedIntents = mutableMapOf<Int, PendingIntent>()
+    private val savedNotificationIdToCachedIntent = mutableMapOf<Long, Int>()
     private val lock = Any()
 
-    fun read(savedNotificationId: Long) = synchronized(lock) {
-        data[savedNotificationId]
+    fun read(savedNotificationId: Long): PendingIntent? = synchronized(lock) {
+        val intentId = savedNotificationIdToCachedIntent[savedNotificationId]
+
+        return if (intentId == null) {
+            null
+        } else {
+            cachedIntents[intentId]
+        }
     }
 
     fun save(savedNotificationId: Long, contentIntent: PendingIntent) = synchronized(lock) {
-        data.put(savedNotificationId, contentIntent)
+        // check if intent already exists
+        val oldCachedIntentId = cachedIntents.entries.find { it.value == contentIntent }?.key
+
+        if (oldCachedIntentId != null) {
+            if (BuildConfig.DEBUG) {
+                Log.d(LOG_TAG, "got intent already")
+            }
+
+            savedNotificationIdToCachedIntent[savedNotificationId] = oldCachedIntentId
+        } else {
+            if (BuildConfig.DEBUG) {
+                Log.d(LOG_TAG, "add new intent")
+            }
+
+            // add intent
+            val newCachedIntentId = nextCachedIntentId++
+            cachedIntents[newCachedIntentId] = contentIntent
+            savedNotificationIdToCachedIntent[savedNotificationId] = newCachedIntentId
+
+            // eventually remove old intent
+            if (cachedIntents.size > CACHE_SIZE) {
+                if (BuildConfig.DEBUG) {
+                    Log.d(LOG_TAG, "clean up cache")
+                }
+
+                val oldestCachedIntentId = cachedIntents.keys.min()
+                cachedIntents.remove(oldestCachedIntentId)
+
+                // remove referencing items
+                savedNotificationIdToCachedIntent
+                        .entries
+                        .filter { it.value == oldestCachedIntentId }
+                        .forEach {
+                            savedNotificationIdToCachedIntent.remove(it.key)
+                        }
+            }
+        }
     }
 }
